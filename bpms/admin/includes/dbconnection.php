6f0b4b95a9be1d3d9a6f8e9aa678aa03d46711f8
JCazores <john.christian.azores2016@gmail.com>
2026-06-08 13:39:59 +0800

    Fix admin dbconnection

--- bpms/admin/includes/dbconnection.php
+++ bpms/admin/includes/dbconnection.php
@@ -1,7 +1,13 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "rfbs");
-if(mysqli_connect_errno()){
-echo "Connection Fail".mysqli_connect_error();
-}
+$host = 'acela.proxy.rlwy.net';
+$port = 58509;
+$user = 'root';
+$pass = 'oCZnrPaBlUHwYSSosvPTWAFRnKiSwQJI';
+$db   = 'rfbs';
 
-  ?>
+$con = mysqli_connect($host, $user, $pass, $db, $port);
+if(mysqli_connect_errno()){
+    echo "Connection Fail: ".mysqli_connect_error();
+    exit();
+}
+?>
--- bpms/includes/dbconnection.php
+++ bpms/includes/dbconnection.php
@@ -1,12 +1,7 @@
 <?php
-$host = getenv('DB_HOST') ?: 'acela.proxy.rlwy.net';
-$port = intval(getenv('DB_PORT') ?: 58509);
-$user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASS') ?: 'oCZnrPaBlUHwYSSosvPTWAFRnKiSwQJI';
-$db   = getenv('DB_NAME') ?: 'rfbs';
-
-$con = mysqli_connect($host, $user, $pass, $db, $port);
+$con=mysqli_connect("localhost", "root", "", "rfbs");
 if(mysqli_connect_errno()){
-    echo "Connection Fail: ".mysqli_connect_error();
+echo "Connection Fail".mysqli_connect_error();
 }
-?>
+
+  ?>
