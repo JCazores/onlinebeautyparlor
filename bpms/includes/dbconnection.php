0cbaab34e07cd3a4fa40bd3668953dd659c50e61
JCazores <john.christian.azores2016@gmail.com>
2026-06-08 13:17:07 +0800

    Fix DB connection for Railway

--- bpms/includes/dbconnection.php
+++ bpms/includes/dbconnection.php
@@ -1,7 +1,12 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "rfbs");
-if(mysqli_connect_errno()){
-echo "Connection Fail".mysqli_connect_error();
-}
+$host = getenv('DB_HOST') ?: 'acela.proxy.rlwy.net';
+$port = intval(getenv('DB_PORT') ?: 58509);
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASS') ?: 'oCZnrPaBlUHwYSSosvPTWAFRnKiSwQJI';
+$db   = getenv('DB_NAME') ?: 'rfbs';
 
-  ?>
+$con = mysqli_connect($host, $user, $pass, $db, $port);
+if(mysqli_connect_errno()){
+    echo "Connection Fail: ".mysqli_connect_error();
+}
+?>
